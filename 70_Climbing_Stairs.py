89659608fa552e0dc1909717381313453016e14a
Asaph Yuan <asaph.yuan@gmail.com>
2016-01-04 23:34:26 -0800

    added 70

---
+++ 70_Climbing_Stairs.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if (n <= 1)
+          return n;
+        return self.climbStairs(n-1) + self.climbStairs(n-2);
